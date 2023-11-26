82e7e6d817e36e142a96b3dc366d1d88b962569c
egor <egorvasilev8087@gmail.com>
2023-11-26 17:46:51 +0300

    0.3Alpha

--- app/src/main/java/com/example/my_health_companion/MainActivity.kt
+++ app/src/main/java/com/example/my_health_companion/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.my_health_companion
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,6 +21,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         var Button1: Button = findViewById(R.id.button12)
+        var textView: TextView = findViewById<TextView>(R.id.imageView6)
+        val mes1 = intent.getStringExtra("mes1")
+        val mes2 = intent.getStringExtra("mes2")
+
+        var str = "Здравствуйте, " + mes1 + " " + mes2 + "!"
+
+        textView.setText(str)
 
         Button1.setOnClickListener{
             val intent = Intent(this,LogActivity::class.java)
--- app/src/main/java/com/example/my_health_companion/RegActivity.kt
+++ app/src/main/java/com/example/my_health_companion/RegActivity.kt
@@ -3,6 +3,8 @@ package com.example.my_health_companion
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,10 +20,27 @@ class RegActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.reg_activity)
-        var Button1: Button = findViewById(R.id.button1)
+        var Button1: ImageButton = findViewById(R.id.regButton3)
+        var Button2: Button = findViewById(R.id.RegButton)
+
+
+
+
 
         Button1.setOnClickListener{
             val intent = Intent(this,LogActivity::class.java)
+            startActivity(intent)
+        }
+
+        Button2.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            val name: EditText = findViewById<EditText>(R.id.name)
+            val patronymic: EditText = findViewById<EditText>(R.id.patronymic)
+
+            val mes1 = name.text.toString()
+            val mes2 = patronymic.text.toString()
+            intent.putExtra("mes1", mes1)
+            intent.putExtra("mes2", mes2)
             startActivity(intent)
         }
     }
